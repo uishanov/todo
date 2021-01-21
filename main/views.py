2e7e78fade9def037177e09534fc3b838b911b2b
Умид Ишанов <u.ishanov@mbf.local>
2021-01-21 11:43:10 +0600

    set static and media urls roots

--- main/views.py
+++ main/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import ToDo
 
-# Create your views here.
+
 def homepage(request):
-    return HttpResponse('Hello world') 
-    # функция
+    return render(request, "index.html")
+
+
+def test(request):
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
+
 
 def second(request):
-    return HttpResponse('This is page test3') 
+    return HttpResponse("test 2 page")
 
-# def third(request):
-#     return HttpResponse(' test2')
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
+
+
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
+
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
--- todo/urls.py
+++ todo/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage,second
-
+from main.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',homepage, name='home'),
-    path("test3/",second),
-    # path("test3/",third)
-]
+    path("", homepage, name="home"),
+    path("test/", test, name="test"),
+    path("test2/", second),
+    path("add-todo/", add_todo, name="add-todo"),
+    path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("close-todo/<id>/", close_todo, name="close-todo"),
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
